3d6e8d0e7deb27df7d13a2d1603ae002a3ee748f
jihoon <atomatam@hanmail.net>
2014-12-04 12:24:13 +0900

    이미지는 잘 안보임

---
+++ Web_Programming_project_v9.1_imageupload_no_view/WebContent/test.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<img src="C:\Users\김지훈\workspace\.metadata\.plugins\org.eclipse.wst.server.core\tmp1\wtpwebapps\Web_Programming_project\upload\20141203191853.png">
+</body>
+</html>
---
+++ Web_Programming_project_v9.1_imageupload_no_view/src/utility/fileManager.java
@@ -0,0 +1,66 @@
+package utility;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+import java.io.*;
+import java.util.Date;
+import java.util.StringTokenizer;
+import java.text.SimpleDateFormat;
+
+import kr.ac.mju.FileDAO;
+import kr.ac.mju.FileDTO;
+
+public class fileManager {
+
+	public static void createFile(FileDTO file) {			///파일을 올리면  이것이 호출 되어저장이 됨.
+		FileDTO tempFile = null;
+		
+		tempFile = FileDAO.getFileFromTitle(file.getFileTitle());
+		if(tempFile==null) {
+			FileDAO.createFile(file);
+		} else {
+			tempFile.setFilePath(file.getFilePath()+"/"+tempFile.getFilePath());
+			tempFile.setFileExplain(file.getFileExplain()+"/"+tempFile.getFileExplain());
+			FileDAO.updateFile(tempFile);
+		}
+		System.out.println(FileDAO.getFileFromTitle(file.getFileTitle()).getFilePath());
+	}
+	
+	public static String getParsedFile(String title) { //writingdto의 title을 이용하여 화면에 뿌려질 파일을 태그를 포함해서 줌.
+		FileDTO file = FileDAO.getFileFromTitle(title);
+		if(file!=null) {
+		StringTokenizer tempImages =  new StringTokenizer(file.getFilePath(), "/" );
+		StringTokenizer tempExplain = new StringTokenizer(file.getFileExplain(), "/");
+		String images[] = new String[tempImages.countTokens()];
+		String explain[] = new String[tempExplain.countTokens()];
+		String result = null;
+		for(int i=0; i<images.length; i++) {
+			images[i] = tempImages.nextToken();
+			explain[i] = tempExplain.nextToken();
+		}
+		explain = getParsingImage(explain);
+		result = sumTwoArray(images, explain);
+		
+		return result;
+		}
+		return null;
+	}
+	
+	public static String[] getParsingImage(String[] images) {
+		for(int i=0; i<images.length; i++) {
+			images[i] = "<img src=\""+images+"\">";
+		}
+		return images;
+	}
+	
+	public static String sumTwoArray(String[] array1, String[] array2) {
+		StringBuffer str = new StringBuffer();
+		for(int i=0; i<array1.length; i++) {
+			str.append(array1[i]+"\r\n");
+			str.append(array2[i]);
+		}
+		
+		return str.toString();
+	}
+}
