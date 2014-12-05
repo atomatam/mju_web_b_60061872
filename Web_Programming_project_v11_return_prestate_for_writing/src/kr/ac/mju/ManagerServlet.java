a6dc37531b1b65bdc1a1fe874440c028db36643c
jihoon <atomatam@hanmail.net>
2014-12-05 18:12:21 +0900

    관리자가 정보 글을 버튼 하나를 클릭해서 이전 상태로 돌림

---
+++ Web_Programming_project_v11_return_prestate_for_writing/src/kr/ac/mju/ManagerServlet.java
@@ -0,0 +1,118 @@
+package kr.ac.mju;
+
+import java.io.*;
+import java.util.*;
+
+import kr.ac.mju.*;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+
+
+
+import utility.*;
+
+/**
+ * Servlet implementation class PageServlet
+ */
+@WebServlet("/ManagerServlet")
+public class ManagerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public static WritingDTO useModify=null;			//정보 수정을 할 경우 쓰는 라이팅디티오 객체를 담아둠. 이것을 쓰지 않으면 콘텐츠가 파싱되서 나옴.writingManager에서	
+    /**														//최초 정보를 읽을 때 초기화 함
+     * @see HttpServlet#HttpServlet()
+     */
+    public ManagerServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String op = request.getParameter("op");
+		String pageLocation = "";
+		System.out.println("오피 : "+op);
+		if(op.equals("logout")) {
+			pageLocation="login.jsp";
+			HttpSession session = request.getSession();
+			session.invalidate();
+			response.setHeader("Cache-Control","no-store");   
+			response.setHeader("Pragma","no-cache");   
+			response.setDateHeader("Expires",0);   
+			if (request.getProtocol().equals("HTTP/1.1")) 
+			        response.setHeader("Cache-Control", "no-cache"); 
+		} else if(op.equals("writingSearch")) {
+			WritingDTO writing = null;
+			writing = writingManager.pasingWritingDTO(request.getParameter("keyword"));
+			if(writing==null) {
+				request.setAttribute("errorMsg", "검색 결과가 없습니다.");
+				pageLocation="error.jsp";
+			} else {
+				String images = fileManager.getParsedFile(writing.getWriting_title());
+				request = requestManager.setRequestWriting(request, writing, images);				//image에 path와 explain이 getParseFile에서 합쳐짐
+				request.setAttribute("isInitSearch", "false");
+				pageLocation = "manager.jsp";
+			}
+		} else if(op.equals("showHistory")) {
+			String date = request.getParameter("date");
+			String historyInfor[] = historyManager.getHistoryInforFromDate(date);
+			request.setAttribute("title", useModify.getWriting_title());
+			request.setAttribute("date", historyInfor[0]);
+			request.setAttribute("writer", historyInfor[1]);
+			request.setAttribute("content", historyInfor[2]);
+			pageLocation = "historyPage.jsp";
+		} else if(op.equals("userSearch")) {
+			
+		} else if(op.equals("deleteContent")) {
+			WritingDTO writing = null;
+			writing = WritingDAO.getWritingDTOFromKeyword(request.getParameter("keyword"));
+			String errorMsg=managingManager.deleteContent(writing);						//위에서 키워드를 이용해 가장 최신의 라이팅을 지우는 함수를 호출함.
+			if(errorMsg!=null) {
+				pageLocation = "managerError.jsp";
+				request.setAttribute("errorMsg", errorMsg);
+				request.setAttribute("keyword", writing.getWriting_title());
+			} else {
+				WritingDTO writingDto = null;
+				writingDto = writingManager.pasingWritingDTO(request.getParameter("keyword"));
+				if(writingDto==null) {
+					request.setAttribute("errorMsg", "검색 결과가 없습니다.");
+					pageLocation="error.jsp";
+				} else {
+					String images = fileManager.getParsedFile(writingDto.getWriting_title());
+					request = requestManager.setRequestWriting(request, writingDto, images);				//image에 path와 explain이 getParseFile에서 합쳐짐
+					request.setAttribute("isInitSearch", "false");
+					
+					pageLocation = "manager.jsp";
+				}
+			}
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher(pageLocation);
+		dispatcher.forward(request,  response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String errorMsg = null;
+		request.setCharacterEncoding("UTF-8");
+		String isLogin = (String) request.getSession().getAttribute("isLogin");
+		String pageLocation = "";
+		String op = request.getParameter("op");
+	    System.out.println("isLogin안의 값 : "+isLogin);
+		pageLocation = "boardMain.jsp";
+		
+		request.setAttribute("errorMsg", errorMsg);
+		System.out.println("최종 pageLocation:" + pageLocation);
+		RequestDispatcher dispatcher = request.getRequestDispatcher(pageLocation);
+		dispatcher.forward(request,  response);
+	}
+}
